f81682496b273161890cbb380e164222af112d7e
Pratik Potadar <pratikpotadar124@gmail.com>
2025-11-30 16:46:06 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+.connect(process.env.Mongo).then(()=>{
+    console.log("Connected to Mongodb")
+    }).catch((err) => {
+        console.log(err);
+    })
+
 
 const app = express(); 
 
